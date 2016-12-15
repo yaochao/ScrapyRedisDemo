5519e0cab2c551299b1a6037b8e79ede87f60deb
yaochao <yaochao365@qq.com>
2016-12-15 16:45:22 +0800

    add mongodbpipeline

--- ScrapyRedisDemo/pipelines.py
+++ ScrapyRedisDemo/pipelines.py
@@ -5,7 +5,21 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymongo
 
 class ScrapyredisdemoPipeline(object):
     def process_item(self, item, spider):
+        return item
+
+class BaikeMongodbPipeline(object):
+    def __init__(self):
+        client = pymongo.MongoClient(host='121.42.202.198', port=27017)
+        db = client['baidu_baike']
+        self.info = db['info']
+
+    def process_item(self, item, spider):
+        try:
+            self.info.insert(item)
+        except:
+            print('insert into mongodb error!!!')
         return item
--- ScrapyRedisDemo/spiders/baikespider.py
+++ ScrapyRedisDemo/spiders/baikespider.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by yaochao on 2016/12/14
+import json
 
 from scrapy_redis.spiders import RedisSpider
-from scrapy import Request
+from scrapy import Request, Selector
 
 class BaikeSpider(RedisSpider):
     name = 'baike'
@@ -11,7 +12,10 @@ class BaikeSpider(RedisSpider):
     custom_settings = {
         'SCHEDULER': 'scrapy_redis.scheduler.Scheduler',
         'DUPEFILTER_CLASS': 'scrapy_redis.dupefilter.RFPDupeFilter',
-        'REDIS_URL': 'redis://121.42.202.198:6379'
+        'REDIS_URL': 'redis://121.42.202.198:6379',
+        'ITEM_PIPELINES': {
+            'ScrapyRedisDemo.pipelines.BaikeMongodbPipeline': 100
+        }
     }
 
     def parse(self, response):
@@ -21,4 +25,23 @@ class BaikeSpider(RedisSpider):
             request = Request(url=url, callback=self.parse)
             yield request
         title = response.xpath('//dd/h1/text()').extract_first()
-        print(title)
+        url = response.url
+        selector = Selector(response)
+        newLemmaId = selector.re(r'newLemmaIdEnc:"(.*?)"')[0] if selector.re(r'newLemmaIdEnc:"(.*?)"') else ''
+        item = {
+            '_id': newLemmaId,
+            'title':title,
+            'url': url,
+            'newLemmaId': newLemmaId,
+        }
+        if newLemmaId:
+            request = Request(url='http://baike.baidu.com/api/lemmapv?id=' + newLemmaId, callback=self.parse_pageview)
+            request.meta['item'] = item
+            yield request
+
+    def parse_pageview(self, response):
+        item = response.meta['item']
+        a_dict = json.loads(response.text)
+        item['pv'] = a_dict['pv']
+        yield item
+        # print item
